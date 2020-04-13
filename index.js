fb44a28b2d08e16dbc6d73b4acf4cbdf2264fe61
nimeresam <nimeresam95@gmail.com>
2020-04-13 22:37:41 +0300

    create server directory

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(require("cors")());
 app.use(require("body-parser").urlencoded({ extended: true }))
 app.use(require("body-parser").json());
 
-app.use("/api", require("./routes/youtube.routes"));
-app.use("/api", require("./routes/soundcloud.routes"));
+app.use("/api", require("./server/routes/youtube.routes"));
+app.use("/api", require("./server/routes/soundcloud.routes"));
 
 const server = require("http").createServer(app);
 server.listen(4895);
